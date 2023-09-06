4ca71a4c64a553b66c42ed65e82de1dcbab1fe2e
Eric Wu <eric3u@outlook.com>
2023-09-07 00:16:14 +0800

    CyberGearKit: add Swift-SocketCAN

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CyberGearUtils",
     dependencies: [
+        .package(url: "https://github.com/AutomotiveSwift/Swift-SocketCAN.git", from: "0.9.0")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -13,7 +14,10 @@ let package = Package(
         .target(name: "CCyberGearProtocol",
                 exclude: ["test"]),
         .target(name: "CyberGearKit",
-                dependencies: [.target(name: "CCyberGearProtocol")]),
+                dependencies: [
+                    .target(name: "CCyberGearProtocol"),
+                    .product(name: "Swift-SocketCAN", package: "Swift-SocketCAN")
+                ]),
         .executableTarget(
             name: "CyberGearUtils",
             dependencies: [
